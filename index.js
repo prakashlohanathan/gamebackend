d650728f1a608f25d509137eeef05568c475068e
prakashlohanathan <prakash2k2mech@gmail.com>
2024-10-20 15:33:05 +0530

    package.json updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { dbConnection } from "./db.js";
-import { saveGame, getGames } from "./Controller/gameController.js"
+import { saveGame, getGames } from "./Controller/gameController.js";
 
 
 //ENV configuration
